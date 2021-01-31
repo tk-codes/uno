7fd81b3926d9bfa98ae9405e4c854ce49b65d658
tk-codes <3751899+tk-codes@users.noreply.github.com>
2021-01-31 13:44:53 +0100

    create player view

--- src/main/java/UNOApp.java
+++ src/main/java/UNOApp.java
@@ -1,9 +1,19 @@
-import ui.AppWindow;
+import domain.game.GameBuilder;
+import ui.AppFrame;
 
 import javax.swing.*;
+import java.util.stream.Collectors;
 
 public class UNOApp {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(AppWindow::new);
+        var game = new GameBuilder()
+            .withPlayer("Player 1")
+            .withPlayer("Player 2")
+            .withPlayer("Player 3")
+            .build();
+
+        SwingUtilities.invokeLater(()->{
+            var frame = new AppFrame(game.getPlayers().collect(Collectors.toList()));
+        });
     }
 }
